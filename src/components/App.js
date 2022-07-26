ec03b7aee06bbced227790bd2acc3a693b2a0f73
gloria oduor <gloriaoduor@gmail.com>
2022-07-26 10:32:59 +0300

    Move app.js to components folder

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="phase2-project\src\download.jpg" className="" alt="logo" />
+        <img src=".\phase2-project\src\download.jpg" className="" alt="film-png" />
         <p>
          Welcome to The Movies Bay!
         </p>
         <input type='text' value=''></input> 
         <button type='submit'>Search</button>
+        <p>Search by <a href='#genres'>Genre</a> instead</p>
       </header>
       <Genres />
     </div>
