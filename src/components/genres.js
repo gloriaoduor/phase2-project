ee10b030023ba40f24cb82711cc59ed31fe5d297
gloria oduor <gloriaoduor@gmail.com>
2022-07-26 10:32:25 +0300

    Initialize and move to components folder

---
+++ src/components/genres.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function Genres(){
+    return (
+        <div className='genrePage'>
+            <h2>
+                GENRES
+            </h2>
+            <input type='text' value=''></input> 
+        <button type='submit'>Search</button>
+        </div>
+    )
+}
+export default Genres;
