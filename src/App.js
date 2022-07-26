76cc2198c02c6955c04f17781962791da2b760fa
gloria oduor <gloriaoduor@gmail.com>
2022-07-26 10:09:07 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Genres from './genres';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="phase2-project\src\download.jpg" className="" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Welcome to The Movies Bay!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type='text' value=''></input> 
+        <button type='submit'>Search</button>
       </header>
+      <Genres />
     </div>
   );
 }
---
+++ src/genres.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Genres(){
+    return (
+        <div>
+            <h2>
+                GENRES
+            </h2>
+        </div>
+    )
+}
+export default Genres;
